72cfc445b640a7373d3725096e8f0b61de6b3677
zjavkovam <zjavkova.m@gmail.com>
2022-03-05 00:56:49 +0100

    allow hosts fix

--- app/views.py
+++ app/views.py
@@ -3,13 +3,17 @@ from django.http import HttpResponse
 import psycopg2
 
 # Create your views here
+from dbs_zadanie.settings import env
+
+
 def message(request):
+    SECRET_KEY = env('SECRET_KEY')
     conn = psycopg2.connect(
-        host = "147.175.150.216",
+        host="147.175.150.216",
         port="5432",
         database="dota2",
-        user="xzjavkova",
-        password="uVo.kur.2.esy"
+        user=env('DATABASE_USER'),
+        password=env('DATABASE_PASS')
     )
 
     cur = conn.cursor()
