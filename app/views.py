49845d30edb54d2b4d6f9ee0c4a49ed223010c0a
zjavkovam <zjavkova.m@gmail.com>
2022-03-05 01:09:29 +0100

    enviromental variables

--- app/views.py
+++ app/views.py
@@ -9,9 +9,9 @@ from dbs_zadanie.settings import env
 def message(request):
     SECRET_KEY = env('SECRET_KEY')
     conn = psycopg2.connect(
-        host="147.175.150.216",
-        port="5432",
-        database="dota2",
+        host=env('DATABASE_HOST'),
+        port=env('DATABASE_PORT'),
+        database=env('DATABASE_NAME'),
         user=env('DATABASE_USER'),
         password=env('DATABASE_PASS')
     )
